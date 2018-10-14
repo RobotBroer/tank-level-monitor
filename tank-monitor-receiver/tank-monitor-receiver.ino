68d627e1c40b313088f63dc7e6807845676bca93
christo semmelink <semmelinkc@gmail.com>
2018-10-14 15:17:23 +0200

    initial commit

---
+++ tank-monitor-receiver/tank-monitor-receiver.ino
@@ -0,0 +1,154 @@
+#include <SoftEasyTransfer.h>
+
+/*   For Arduino 1.0 and newer, do this:   */
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11);
+
+SoftEasyTransfer ET; 
+SoftEasyTransfer SX;
+
+const int RELAY = 7; 
+const int MINIMUM = 50;
+
+
+struct RECEIVE_DATA_STRUCTURE{
+
+  int16_t distance = 55;
+  byte randomNUM;
+};
+
+struct SEND_DATA_STRUCTURE{
+
+  byte randomSEND;
+};
+
+RECEIVE_DATA_STRUCTURE mydata;
+SEND_DATA_STRUCTURE mydataSEND;
+
+const int RGB[] = {3,4,5};
+
+int analogInPin = A1;
+int sensorValue = 0;        // value read from the carrier board     
+unsigned long msec = 0;
+float timeSec = 0.0;
+int sample = 0;
+float totalCharge = 0.0;
+float averageAmps = 0.0;
+float ampSeconds = 0.0;
+float ampHours = 0.0;
+float amps = 0.0;
+
+
+const boolean RED[] = {HIGH, LOW, LOW};    
+const boolean GREEN[] = {LOW, HIGH, LOW};
+const boolean BLUE[] = {LOW, LOW, HIGH};
+const boolean WHITE[] = {HIGH, HIGH, HIGH};
+const boolean DOWN[] = {LOW, LOW, LOW};
+
+unsigned long timer = 0;
+unsigned long heartbeat = 15000;
+
+void setColour (int*RGB, const boolean*colour)
+// Sets colour of RGB to defined colours. 
+{  
+  for(int i = 0; i < 3; i++)
+  {
+    digitalWrite(RGB[i], colour[i]); 
+  } 
+}
+
+void setup(){
+
+  pinMode(RELAY, OUTPUT);
+  digitalWrite(RELAY, LOW);
+  
+  for(int i=0; i<3; i++)
+    {
+      pinMode(RGB[i], OUTPUT);
+    }
+    
+  mySerial.begin(9600);
+
+  SX.begin(details(mydataSEND), &mySerial);
+  ET.begin(details(mydata), &mySerial);
+  
+  
+  Serial.begin(9600);
+  
+  timer = millis();
+  setColour(RGB,BLUE);
+
+}
+
+void loop(){
+
+   
+  if(millis() - timer > heartbeat){
+    setColour(RGB,RED);
+    digitalWrite(RELAY, LOW);
+  }
+
+   if(ET.receiveData()){
+      Serial.print("distance = ");     
+      Serial.println(mydata.distance); 
+      Serial.println("");  
+      timer = millis();  
+      setColour(RGB,GREEN);
+      mydataSEND.randomSEND = mydata.randomNUM;
+      SX.sendData();
+      delay(500);
+   }
+
+  if(mydata.distance < MINIMUM){
+    digitalWrite(RELAY, HIGH);
+  }
+  else{
+    digitalWrite(RELAY, LOW);
+  }
+
+ /*
+  int sampleAmpVal = 0;
+  float avgSAV = 0;
+
+  for (int x = 0; x < 10; x++){ // run through loop 10x
+
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);  
+  sampleAmpVal = sampleAmpVal + sensorValue; // add samples together
+  delay (100); // let ADC settle before next sample
+  }
+  avgSAV = sampleAmpVal / 10;
+
+  // convert to milli amps
+  amps = (avgSAV  - 512)  / 37.888; 
+
+  sample = sample + 1;
+
+  msec = millis();
+ 
+  timeSec = (float) msec / 1000.0;
+ 
+  totalCharge = totalCharge + amps;
+ 
+  averageAmps = totalCharge / sample;
+
+  ampSeconds = averageAmps*timeSec;
+  
+  ampHours = ampSeconds/3600;
+
+  Serial.print("avgSAV= ");     
+  Serial.println(avgSAV); 
+  Serial.println("");
+  
+  Serial.print("Current (amps) = ");     
+  Serial.println(amps); 
+  Serial.println("");
+
+  Serial.print("Average (amps) = ");     
+  Serial.println(averageAmps); 
+  Serial.println("");
+
+  
+  delay(2000);
+ */
+}
---
+++ tank-monitor-transmitter/tank-monitor-transmitter.ino
@@ -0,0 +1,118 @@
+#include <SoftEasyTransfer.h>
+#include <Narcoleptic.h>
+
+
+SoftwareSerial mySerial(10, 11);
+
+#define ECHOPIN 2// Pin to receive echo pulse
+
+#define TRIGPIN 3// Pin to send trigger pulse
+
+#define GATE 5
+
+
+//create object
+SoftEasyTransfer ET; 
+SoftEasyTransfer SX;
+
+struct SEND_DATA_STRUCTURE{
+
+  int16_t distance;
+  byte randomNUM;
+};
+
+struct RECEIVE_DATA_STRUCTURE{
+
+  byte randomSEND;
+};
+
+//give a name to the group of data
+SEND_DATA_STRUCTURE mydata;
+RECEIVE_DATA_STRUCTURE mydataRECEIVE;
+
+
+
+int getDistance() {
+
+  
+  pinMode(TRIGPIN, OUTPUT);
+  digitalWrite(ECHOPIN, HIGH);
+  
+  digitalWrite(TRIGPIN, LOW); // Set the trigger pin to low for 2uS
+
+  delayMicroseconds(2);
+
+  digitalWrite(TRIGPIN, HIGH); // Send a 10uS high to trigger ranging
+
+  delayMicroseconds(10);
+
+  digitalWrite(TRIGPIN, LOW); // Send pin low again
+
+  int distance = pulseIn(ECHOPIN, HIGH,26000); // Read in times pulse
+
+  distance= distance/58;
+
+  //Serial.print(F("distance: "));
+ // Serial.println(distance);
+
+  digitalWrite(ECHOPIN, LOW);
+  
+
+  return distance;
+
+}
+
+void setup(){
+  mySerial.begin(9600);
+    Serial.begin(9600);
+  //start the library, pass in the data details and the name of the serial port.
+  ET.begin(details(mydata), &mySerial);
+  SX.begin(details(mydataRECEIVE), &mySerial);
+  
+ // pinMode(13, OUTPUT);
+
+  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGPIN, OUTPUT);
+  digitalWrite(ECHOPIN, HIGH);
+
+  pinMode(GATE, OUTPUT);
+  digitalWrite(GATE, LOW);
+  
+}
+
+void loop(){
+  //this is how you access the variables. [name of the group].[variable name]
+  
+  
+  digitalWrite(GATE, HIGH);
+
+  delay(500);
+  
+  mydata.distance = getDistance();
+  mydata.randomNUM = random(1,128);
+
+  unsigned long timer = millis();
+  ET.sendData();
+
+  delay(500);
+
+  if(SX.receiveData()){
+      unsigned long timer2 = millis();
+      if(mydata.randomNUM == mydataRECEIVE.randomSEND){
+
+        Serial.print(F("time: " ));
+        Serial.println(timer2 - timer);
+            
+      
+        pinMode(TRIGPIN, INPUT);
+        digitalWrite(GATE, LOW);
+  
+        for(int i = 0; i < 10; i++){
+           Narcoleptic.delay(1000);
+      }
+    } 
+  }
+
+  delay(500);//
+  
+}
